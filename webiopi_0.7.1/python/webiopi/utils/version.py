088d243cfc4cd67bd8175254e173651210f97e2e
Thor Watanabe <thormac@gmail.com>
2016-09-19 09:51:51 +0900

    close #26

--- webiopi_0.7.1/python/webiopi/utils/version.py
+++ webiopi_0.7.1/python/webiopi/utils/version.py
@@ -7,8 +7,8 @@ PYTHON_MAJOR    = sys.version_info.major
 BOARD_REVISION  = 0
 
 _MAPPING = [[], [], [], []]
-_MAPPING[1] = ["V33", "V50", 0, "V50", 1, "GND", 4, 14, "GND", 15, 17, 18, 21, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7]
-_MAPPING[2] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7]
+_MAPPING[1] = ["V33", "V50", 0, "V50", 1, "GND", 4, 14, "GND", 15, 17, 18, 21, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7, "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC"]
+_MAPPING[2] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7, "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC"]
 _MAPPING[3] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7, "DNC", "DNC" , 5, "GND", 6, 12, 13, "GND", 19, 16, 26, 20, "GND", 21]
 
 
@@ -24,7 +24,7 @@ try:
             cpurev = int(hex_cpurev, 16)
             if cpurev < 4:
               BOARD_REVISION = 1
-            elif cpurev < 7:
+            elif cpurev < 16:
               BOARD_REVISION = 2
             else:
               BOARD_REVISION = 3
