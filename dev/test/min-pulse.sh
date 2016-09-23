729eb61b818a4c646a90ddf6a9f3f64932bd3bc7
Thor Watanabe <thormac@gmail.com>
2016-09-23 22:33:33 +0900

    #27

--- dev/test/min-pulse.sh
+++ dev/test/min-pulse.sh
@@ -7,11 +7,15 @@ ipadr=127.0.0.1     # webiopi server ip address
 port=8000           # webiopi server port number
 pin=4               # GPIO port number
 freq=0.01           # frequency [100s]
+wait=300            # sec
 
 if [ "$1" != "" ] ; then
     freq=$1
 fi
 echo "Frequency is $freq [Hz]"
+if [ "$2" != "" ] ; then
+    wait=$2
+fi
 
 # function
 curl -X POST -u $user:$pass http://$ipadr:$port/GPIO/$pin/function/pwm && echo
@@ -23,6 +27,6 @@ curl -u $user:$pass http://$ipadr:$port/GPIO/$pin/freq && echo
 curl -X POST -u $user:$pass http://$ipadr:$port/GPIO/$pin/pulseRatio/0.5 && echo
 curl -u $user:$pass http://$ipadr:$port/GPIO/$pin/pulse && echo
 # wait for 300sec
-sleep 300
+sleep $wait
 # disable pwm.
 curl -X POST -u $user:$pass http://$ipadr:$port/GPIO/$pin/function/in && echo
