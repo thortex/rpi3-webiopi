ab221ff118ef6cb838f1212d8ba5e41978601fd7
Thor Watanabe <thormac@gmail.com>
2016-10-31 20:05:28 +0900

    #33

--- webiopi_0.7.1/python/webiopi/utils/version.py
+++ webiopi_0.7.1/python/webiopi/utils/version.py
@@ -1,8 +1,13 @@
 import re
 import sys
 
+try:
+    import _webiopi.GPIO as GPIO
+except:
+    pass
+
 VERSION         = '0.7.1'
-VERSION_STRING  = "WebIOPi/%s/Python%d.%d" % (VERSION, sys.version_info.major, sys.version_info.minor)
+VERSION_STRING  = "YA-WebIOPi/%s/Python%d.%d" % (VERSION, sys.version_info.major, sys.version_info.minor)
 PYTHON_MAJOR    = sys.version_info.major
 BOARD_REVISION  = 0
 
@@ -11,24 +16,8 @@ _MAPPING[1] = ["V33", "V50", 0, "V50", 1, "GND", 4, 14, "GND", 15, 17, 18, 21, "
 _MAPPING[2] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7, "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC", "DNC"]
 _MAPPING[3] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7, "DNC", "DNC" , 5, "GND", 6, 12, 13, "GND", 19, 16, 26, 20, "GND", 21]
 
-
 try:
-    with open("/proc/cpuinfo") as f:
-        rc = re.compile("Revision\s*:\s(.*)\n")
-        info = f.read()
-        result = rc.search(info)
-        if result != None:
-            hex_cpurev = result.group(1)
-            if hex_cpurev.startswith("1000"):
-                hex_cpurev = hex_cpurev[-4:]
-            cpurev = int(hex_cpurev, 16)
-            if cpurev < 4:
-              BOARD_REVISION = 1
-            elif cpurev < 16:
-              BOARD_REVISION = 2
-            else:
-              BOARD_REVISION = 3
-
+    BOARD_REVISION = GPIO.BOARD_REVISION
 except:
     pass
 
